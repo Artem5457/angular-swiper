aaa4e0a162d5431dac038047d36ef67d5fe62026
Artem Bolshak <Artem.Bolshak@mht.priv>
2022-01-17 15:18:19 +0200

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SwiperOptions } from 'swiper';
+// import Swiper core and required modules
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-swiper';
+  config: SwiperOptions = {
+    spaceBetween: 24,
+    slidesPerView: 3,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    observer: true,
+    observeParents: true,
+  }
+
+  onSwiper(swiper) {
+    console.log(swiper);
+  }
+  onSlideChange() {
+    console.log('slide change');
+  }
 }
